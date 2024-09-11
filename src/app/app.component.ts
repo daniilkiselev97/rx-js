f6b7692f68007cf01fd167245dcedfd24e22624e
Daniil Kiselev <dvkiselev@fil-it.ru>
2024-09-11 17:52:57 +0300

    task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { concatMap, first, fromEvent, interval, map, mergeMap, Observable, scan, startWith, Subject, switchMap, take, takeUntil, takeWhile, tap, timer, withLatestFrom } from 'rxjs';
+
+interface ClickEvent {
+  x: number;
+  y: number;
+}
+
+interface CombinedEvent {
+  x: number;
+  y: number;
+  time: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'rx-js';
+export class AppComponent implements OnInit {
+  //task 1
+  // public source$!: Observable<number>;
+
+  // ngOnInit(): void {
+  //   this.source$ = interval(1000).pipe(
+  //     map(value => value + 1),
+  //     switchMap(value =>
+  //       interval(value % 2 === 0 ? 500 : 1000).pipe(map(() => value))
+  //     )
+  //   );
+
+  //   this.source$.subscribe(value => console.log(value));
+  // }
+
+
+
+
+  //task 2
+  clicks$!: Observable<ClickEvent>;
+  timer$!: Observable<number>;
+  combined$!: Observable<CombinedEvent>;
+
+  lastClick: CombinedEvent | null = null;
+
+  ngOnInit(): void {
+    this.clicks$ = fromEvent<MouseEvent>(document, 'click').pipe(
+      map(event => ({ x: event.clientX, y: event.clientY }))
+    );
+
+    this.timer$ = interval(1000);
+
+    this.combined$ = this.clicks$.pipe(
+      withLatestFrom(this.timer$),
+      map(([click, time]) => ({ ...click, time }))
+    );
+
+    this.combined$.subscribe(event => {
+      this.lastClick = event;
+    });
+  }
+
 }
+
+
+
+
