d4eda58deb293250768425f2c6bd34f43257a901
Daniil Kiselev <dvkiselev@fil-it.ru>
2024-09-11 23:52:52 +0300

    feat task 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { concatMap, first, fromEvent, interval, map, mergeMap, Observable, scan, startWith, Subject, switchMap, take, takeUntil, takeWhile, tap, timer, withLatestFrom } from 'rxjs';
+import { catchError, concat, concatMap, delay, finalize, first, fromEvent, iif, interval, map, mergeMap, Observable, of, onErrorResumeNext, retry, retryWhen, scan, startWith, Subject, switchMap, take, takeUntil, takeWhile, tap, throttleTime, throwError, timer, withLatestFrom } from 'rxjs';
 
 interface ClickEvent {
   x: number;
@@ -40,21 +40,69 @@ export class AppComponent implements OnInit {
 
 
   //task 2
-  combined$!: Observable<CombinedEvent>;
+  // combined$!: Observable<CombinedEvent>;
+
+  // ngOnInit(): void {
+  //   const timer$ = interval(1000);
+
+  //   this.combined$ = fromEvent<MouseEvent>(document, 'click').pipe(
+  //     map(event => ({ x: event.clientX, y: event.clientY })),
+  //     withLatestFrom(timer$),
+  //     map(([click, time]) => ({ ...click, time }))
+  //   );
+
+  // }
+
+
+  //task 3
+  // data$!: Observable<number[]>;
+
+  // ngOnInit(): void {
+  //   const rawData$: Observable<number> = this.simulateDataStream();
+
+  //   this.data$ = rawData$.pipe(
+  //     tap((res) => console.log('before map:', res)),
+  //     map(value => [value * 2]),
+  //     startWith([]),
+  //     catchError(err => {
+  //       console.error('Error occurred:', err); 
+  //       return of([-2]); 
+  //     }),
+  //     scan((acc: number[], curr: number[]) => {
+  //       console.log('acc:', acc)
+  //       console.log('curr:', curr)
+  //       return acc.concat(curr)
+  //     }),
+  //   );
+  // }
+
+  // simulateDataStream(): Observable<number> {
+  //   const values = [1, 2, 3, 4, 5];
+
+  //   return of(...values).pipe(
+  //     concatMap((val, index) => {
+  //       if (index === 3) {
+  //         return throwError(() => new Error('Ошибка на индексе 3')).pipe(delay(1000));
+  //       }
+  //       console.log('val:', val)
+  //       return of(val).pipe(delay(1000));
+  //     }),
+  //   );
+  // }
+
+  //task 4
+  result$!: Observable<string>;
 
   ngOnInit(): void {
-    const timer$ = interval(1000);
-
-    this.combined$ = fromEvent<MouseEvent>(document, 'click').pipe(
-      map(event => ({ x: event.clientX, y: event.clientY })),
-      withLatestFrom(timer$),
-      map(([click, time]) => ({ ...click, time }))
+    this.result$ = fromEvent<MouseEvent>(document, 'click').pipe(
+      throttleTime(2000), 
+      switchMap(() => timer(0, 2000).pipe( 
+        takeUntil(timer(5000)) 
+      )),
+      map(() => `Клик зарегистрирован в ${new Date().toLocaleTimeString()}`), 
+      startWith('Ожидание кликов...') 
     );
-
   }
-
 }
-
-
 
 
