bcfa93d91c29833d672437f5fb3ab7d835a6527f
Daniil Kiselev <dvkiselev@fil-it.ru>
2024-09-13 00:25:48 +0300

    fix: task 3 final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,23 +23,23 @@ interface CombinedEvent {
 })
 export class AppComponent implements OnInit {
   //task 1
-//   public source$!: Observable<string>;
-//   public testString: string = 'test';
+  //   public source$!: Observable<string>;
+  //   public testString: string = 'test';
 
-//   ngOnInit(): void {
-//     this.source$ = interval(1000).pipe(
-//       switchMap((value) => {
-//         if (value % 2 === 0) {
-//           return timer(0, 500).pipe(map(() => this.testString + value));
-//         } else {
-//           return timer(500, 500).pipe(map(() => this.testString + value));
-//         }
-//       })
-//     );
+  //   ngOnInit(): void {
+  //     this.source$ = interval(1000).pipe(
+  //       switchMap((value) => {
+  //         if (value % 2 === 0) {
+  //           return timer(0, 500).pipe(map(() => this.testString + value));
+  //         } else {
+  //           return timer(500, 500).pipe(map(() => this.testString + value));
+  //         }
+  //       })
+  //     );
 
-//     this.source$.subscribe((res)=>console.log(res));
-//   }
-// }
+  //     this.source$.subscribe((res)=>console.log(res));
+  //   }
+  // }
 
 
 
@@ -60,55 +60,57 @@ export class AppComponent implements OnInit {
 
 
   //task 3
-  data$!: Observable<number[]>;
+//   data$!: Observable<number[]>;
 
-  ngOnInit(): void {
-    const rawData$: Observable<number> = this.simulateDataStream();
+//   ngOnInit(): void {
+//     const rawData$: Observable<number> = this.simulateDataStream();
 
-    this.data$ = rawData$.pipe(
-      tap((res) => console.log('before map:', res)),
-      map(value => [value * 2]),
-      startWith([]),
-      catchError(err => {
-        console.error('Error occurred:', err); 
-        return of([-2]); 
-      }),
-      scan((acc: number[], curr: number[]) => {
-        console.log('acc:', acc)
-        console.log('curr:', curr)
-        return acc.concat(curr)
-      }),
-    );
-  }
+//     this.data$ = rawData$.pipe(
+//       tap((res) => console.log('before map:', res)),
+//       map(value => [value * 2]),
+//       startWith([]),
+//       scan((acc: number[], curr: number[]) => {
+//         console.log('acc:', acc);
+//         console.log('curr:', curr);
+//         return acc.concat(curr);
+//       }),
+//     );
+//   }
 
-  simulateDataStream(): Observable<number> {
-    const values = [1, 2, 3, 4, 5];
+//   simulateDataStream(): Observable<number> {
+//     const values = [1, 2, 3, 4, 5];
 
-    return of(...values).pipe(
-      concatMap((val, index) => {
-        if (index === 3) {
-          return throwError(() => new Error('Ошибка на индексе 3')).pipe(delay(1000));
-        }
-        console.log('val:', val)
-        return of(val).pipe(delay(1000));
-      }),
-    );
-  }
-}
+//     return of(...values).pipe(
+//       concatMap((val, index) => {
+//         if (index === 3) {
+//           console.error('Ошибка на индексе 3');
+//           return throwError(() => new Error('Ошибка на индексе 3')).pipe(
+//             delay(1000),
+//             catchError(() => of(-1))
+//           );
+//         }
+//         console.log('val:', val);
+//         return of(val).pipe(delay(1000));
+//       }),
+//     );
+//   }
 
-  //task 4
-  //   result$!: Observable<string>;
 
-  //   ngOnInit(): void {
-  //     this.result$ = fromEvent<MouseEvent>(document, 'click').pipe(
-  //       throttleTime(2000), 
-  //       switchMap(() => timer(0, 2000).pipe( 
-  //         takeUntil(timer(5000)) 
-  //       )),
-  //       map(() => `Клик зарегистрирован в ${new Date().toLocaleTimeString()}`), 
-  //       startWith('Ожидание кликов...') 
-  //     );
-  //   }
-  // }
+// }
+
+//task 4
+//   result$!: Observable<string>;
+
+//   ngOnInit(): void {
+//     this.result$ = fromEvent<MouseEvent>(document, 'click').pipe(
+//       throttleTime(2000),
+//       switchMap(() => timer(0, 2000).pipe(
+//         takeUntil(timer(5000))
+//       )),
+//       map(() => `Клик зарегистрирован в ${new Date().toLocaleTimeString()}`),
+//       startWith('Ожидание кликов...')
+//     );
+//   }
+// }
 
 
